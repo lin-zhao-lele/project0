e2a407e3e3b593999d4505978b61cb3c1a4d09dc
lzhao <lzhao@pcienergysolutions.com>
2025-08-05 18:12:07 -0400

    remove old XGBoost

--- models/LSTM/predict.py
+++ models/LSTM/predict.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import pandas as pd
 import torch
+import torch.nn as nn
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score, precision_score, \
     recall_score, f1_score, roc_auc_score
